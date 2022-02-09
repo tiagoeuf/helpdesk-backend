bdf63930425c947c56be7751213c7eb597aab1b6
tiagoeuf <eufr4sia@gmail.com>
2022-02-09 17:14:31 +0000

    endpoint findAll para tecnico

--- src/main/java/com/tiago/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/tiago/helpdesk/resources/TecnicoResource.java
@@ -1,5 +1,8 @@
 package com.tiago.helpdesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,17 +17,23 @@ import com.tiago.helpdesk.services.TecnicoService;
 @RestController
 @RequestMapping(value = "/tecnicos")
 public class TecnicoResource {
-	
-	//localhost:8080/tecnicos/1
-	
+
+	// localhost:8080/tecnicos/1
+
 	@Autowired
 	private TecnicoService service;
-	
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
 		Tecnico obj = this.service.findById(id);
 		return ResponseEntity.ok().body(new TecnicoDTO(obj));
+	}
+
+	@GetMapping
+	public ResponseEntity<List<TecnicoDTO>> findAll() {
+		List<Tecnico> list = service.findAll();
+		List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
