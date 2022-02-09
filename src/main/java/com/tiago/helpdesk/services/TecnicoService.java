df58a24d006b2a196e1defa96c8f06246c92ea4b
tiagoeuf <eufr4sia@gmail.com>
2022-02-09 17:34:45 +0000

    endpoint create para Tecnico

--- src/main/java/com/tiago/helpdesk/services/TecnicoService.java
+++ src/main/java/com/tiago/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tiago.helpdesk.domain.Tecnico;
+import com.tiago.helpdesk.domain.dto.TecnicoDTO;
 import com.tiago.helpdesk.repositories.TecnicoRepository;
 import com.tiago.helpdesk.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,11 @@ public class TecnicoService {
 
 	public List<Tecnico> findAll() {
 		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
 }
