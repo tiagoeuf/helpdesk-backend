8be5b2d45a3e322007bf457a849a1495f5fee0c8
tiagoeuf <eufr4sia@gmail.com>
2022-02-10 11:20:16 +0000

    Validation nos campos de criaçao de novo user

--- src/main/java/com/tiago/helpdesk/domain/dto/TecnicoDTO.java
+++ src/main/java/com/tiago/helpdesk/domain/dto/TecnicoDTO.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.tiago.helpdesk.domain.Tecnico;
 import com.tiago.helpdesk.domain.enums.Perfil;
@@ -15,11 +17,17 @@ public class TecnicoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	protected Integer id;
+	
+	@NotNull(message = "O campo Nome é requerido")
 	protected String nome;
 
+	@NotNull(message = "O campo Cpf é requerido")
 	protected String cpf;
-
+	
+	@NotNull(message = "O campo Email é requerido")
 	protected String email;
+	
+	@NotNull(message = "O campo Senha é requerido")
 	protected String senha;
 
 	protected Set<Integer> perfis = new HashSet<>();
