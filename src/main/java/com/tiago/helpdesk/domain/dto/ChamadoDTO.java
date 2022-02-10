d825afc3714b163fd887cc18c28eed9f05d567ac
tiagoeuf <eufr4sia@gmail.com>
2022-02-10 17:20:45 +0000

    create para Chamado

--- src/main/java/com/tiago/helpdesk/domain/dto/ChamadoDTO.java
+++ src/main/java/com/tiago/helpdesk/domain/dto/ChamadoDTO.java
@@ -3,6 +3,8 @@ package com.tiago.helpdesk.domain.dto;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.tiago.helpdesk.domain.Chamado;
 
@@ -19,11 +21,17 @@ public class ChamadoDTO implements Serializable {
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
 	
+	@NotNull(message = "O campo Prioridade é requerido")
 	private Integer prioridade;
+	@NotNull(message = "O campo Status é requerido")
 	private Integer status;
+	@NotNull(message = "O campo Titulo é requerido")
 	private String titulo;
+	@NotNull(message = "O campo Observações é requerido")
 	private String observacoes;
+	@NotNull(message = "O campo Tecnico é requerido")
 	private Integer tecnico;
+	@NotNull(message = "O campo Cliente é requerido")
 	private Integer cliente;
 	private String nomeTecnico;
 	private String nomeCliente;
